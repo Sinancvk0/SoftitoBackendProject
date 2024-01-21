96e9edea21dae9115dba39112cb140984dd13870
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-01-22 00:06:10 +0300

    Add project files.

---
+++ SoftitoBackendProject/Program.cs
@@ -0,0 +1,52 @@
+﻿using Example;
+using Newtonsoft.Json;
+using SoftitoBackendProject.Datas;
+
+var personalUsers = JsonConvert.DeserializeObject<IList<Personal>>(PersonalDatas.PersonalJson);
+var studentUsers = JsonConvert.DeserializeObject<IList<Student>>(StudentDatas.StudentJson);
+var jobberUsers = JsonConvert.DeserializeObject<IList<Jobber>>(JobberDatas.JobberJson);
+
+IDictionary<string, IList<string>> indexes = new Dictionary<string, IList<string>>();
+IDictionary<string, IUser> fastList = new Dictionary<string, IUser>();
+
+fastList.AddToDictionary(personalUsers.Select(user => (user as IUser)).ToList(), indexes);
+fastList.AddToDictionary(studentUsers.Select(user => (user as IUser)).ToList(), indexes);
+fastList.AddToDictionary(jobberUsers.Select(user => (user as IUser)).ToList(), indexes);
+
+
+var findAll = FindByIndex("ejeannequin4z");
+Console.WriteLine(JsonConvert.SerializeObject(findAll));
+Console.WriteLine();
+
+
+
+
+/*
+Console.WriteLine(JsonConvert.SerializeObject(fastList));
+Console.WriteLine();
+*/
+
+Console.WriteLine(JsonConvert.SerializeObject(indexes));
+Console.ReadKey();
+
+
+IList<IUser>? FindByIndex(string search)
+{
+    if (indexes.ContainsKey(search))
+    {
+        var findedKeys = indexes[search];
+        return findedKeys.Select(key => fastList[key]).ToList();
+    }
+    return null;
+}
+
+
+/*
+var findAll = FindByIndex("Dugall");
+Console.WriteLine(JsonConvert.SerializeObject(findAll));
+*/
+
+/*
+var findedListWithPredicate = personalUsers.FindAll(user => user.FirstName == "Dugall" || user.LastName == "Dugall");
+Console.WriteLine(JsonConvert.SerializeObject(findedListWithPredicate));
+*/
